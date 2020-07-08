48121c1777d436b52cb03c35d98f37809cfbd9e6
Tiffhanie M Ramos <citmr@wakefern.com>
2020-07-08 12:29:51 -0400

    added more people in event object

--- src/app/events/events-app.component.ts
+++ src/app/events/events-app.component.ts
@@ -15,22 +15,78 @@ import {Component} from '@angular/core';
 // Create class: EventsListComponent
 // Note: Class Name = File Name
 export class EventsAppComponent {
-  funFacts = {
-    id: 1,
-    name: 'Scarlet',
-    day: '7/6/2020',
-    time: '01:24 pm',
-    hungerPercent: 44.55,
-    imageURL: {
-      icon: '../assets/images/Blissey.png',
-      starter: '../assets/images/Piplup.jpg',
-      favorite: '../assets/images/BlisseyNurse.jpg',
-      legendary: '../assets/images/Jirachi.jpg',
+  funFacts = [
+    {
+      id: 1,
+      name: 'Scarlet',
+      day: '7/6/2020',
+      time: '01:24 pm',
+      hungerPercent: 44.55,
+      imageURL: {
+        icon: '../assets/images/Blissey.png',
+        starter: '../assets/images/Piplup.jpg',
+        favorite: '../assets/images/BlisseyNurse.jpg',
+        legendary: '../assets/images/Jirachi.jpg',
+      },
+      pokemon: {
+        starter: 'Piplup',
+        favorite: 'Blissey',
+        legendary: 'Jirachi'
+      }
     },
-    pokemon: {
-      starter: 'Piplup',
-      favorite: 'Blissey',
-      legendary: 'Jirachi'
+    {
+      id: 2,
+      name: 'Cynthia',
+      day: '7/6/2020',
+      time: '01:24 pm',
+      hungerPercent: 44.55,
+      imageURL: {
+        icon: '../assets/images/Cynthia.jpg',
+        starter: '../assets/images/Bulbasaur.jpg',
+        favorite: '../assets/images/Ninetails.jpg',
+        legendary: '../assets/images/Mew.jpg',
+      },
+      pokemon: {
+        starter: 'Bulbasaur',
+        favorite: 'Ninetails',
+        legendary: 'Mew'
+      }
+    },
+    {
+      id: 3,
+      name: 'Misty',
+      day: '7/6/2020',
+      time: '01:24 pm',
+      hungerPercent: 44.55,
+      imageURL: {
+        icon: '../assets/images/Misty.png',
+        starter: '../assets/images/Totodile.png',
+        favorite: '../assets/images/Dragonite.png',
+        legendary: '../assets/images/Entei.jpg',
+      },
+      pokemon: {
+        starter: 'Totodile',
+        favorite: 'Dragonite',
+        legendary: 'Entei'
+      }
+    },
+    {
+      id: 4,
+      name: 'May',
+      day: '7/6/2020',
+      time: '01:24 pm',
+      hungerPercent: 44.55,
+      imageURL: {
+        icon: '../assets/images/May.jpg',
+        starter: '../assets/images/Torchic.jpg',
+        favorite: '../assets/images/Milotic.png',
+        legendary: '../assets/images/Manaphy.jpg',
+      },
+      pokemon: {
+        starter: 'Torchic',
+        favorite: 'Milotic',
+        legendary: 'Manaphy'
+      }
     }
-  };
+  ];
 }
